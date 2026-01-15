e05c615328573574a024fbfc75fd60b2528aea70
Abhinay Rana <abhinayrana46@gmail.com>
2026-01-15 14:10:44 +0530

    "Solved the 169th problem"

---
+++ src/ArrayList/OneSixNine.java
@@ -0,0 +1,26 @@
+package ArrayList;
+
+public class OneSixNine {
+    class Solution {
+        public int majorityElement(int[] nums) {
+            int counter = 0;
+            int current = 0;
+            for(int i = 0; i < nums.length; i++)
+            {
+                if(counter == 0)
+                {
+                    current = nums[i];
+                }
+                if(nums[i] == current)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+            }
+            return current;
+        }
+    }
+}
