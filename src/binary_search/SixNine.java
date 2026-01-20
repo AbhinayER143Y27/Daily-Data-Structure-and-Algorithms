66a51752bd834208226ce93d7b5960ab643ffd3c
Abhinay Rana <abhinayrana46@gmail.com>
2026-01-20 14:57:18 +0530

    "Solved 69th question"

---
+++ src/binary_search/SixNine.java
@@ -0,0 +1,28 @@
+package binary_search;
+
+public class SixNine {
+    class Solution {
+        public int mySqrt(int x) {
+            long low = 0;
+            long high = x;
+            long ans = 0;
+
+            while(low <= high)
+            {
+                long mid = low + (high - low)/2;
+
+                if(mid * mid <= x)
+                {
+                    ans = mid;
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return (int) ans;
+
+        }
+    }
+}
