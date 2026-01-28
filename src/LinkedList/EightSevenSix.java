ba94267deca2a30697242926c73034de13851822
Abhinay Rana <abhinayrana46@gmail.com>
2026-01-28 10:33:07 +0530

    "Started the new Data Structures"

---
+++ src/LinkedList/EightSevenSix.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class EightSevenSix {
+    public ListNode middleNode(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
