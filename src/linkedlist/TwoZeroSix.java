0abc0c2e16c6a4d5f28fff37439a904bbc8e893a
Abhinay Rana <abhinayrana46@gmail.com>
2026-01-29 15:34:50 +0530

    "Did the 206th question "

---
+++ src/linkedlist/TwoZeroSix.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+public class TwoZeroSix {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;   // This will become the new head (starts as null)
+        ListNode curr = head;   // The node we are currently processing
+
+        while (curr != null) {
+            ListNode front = curr.next; // 1. Save the next node (so we don't lose the chain)
+            curr.next = prev;           // 2. Point backwards! (Cut the old link)
+            prev = curr;                // 3. Move 'prev' forward
+            curr = front;               // 4. Move 'curr' forward
+        }
+
+        // At the end, 'prev' is sitting at the new head (the old last node)
+        return prev;
+    }
+}
