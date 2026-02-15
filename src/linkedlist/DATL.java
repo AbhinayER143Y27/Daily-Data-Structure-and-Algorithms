bd1e64c0868458d3a5c7714be8be6a531427289f
Abhinay Rana <abhinayrana46@gmail.com>
2026-02-15 12:18:54 +0530

    "Did the deletion at the last of a ListNode."

---
+++ src/linkedlist/DATL.java
@@ -0,0 +1,36 @@
+package linkedlist;
+
+import static linkedlist.DATM.*;
+
+public class DATL {
+    public static void main (String args[])
+    {
+        ListNode head = new ListNode(10);
+        ListNode tail = head;
+        tail = addon(tail, 20);
+        tail = addon(tail, 30);
+        tail = addon(tail, 40);
+        tail = addon(tail, 50);
+        tail = addon(tail, 60);
+        tail = addon(tail, 70);
+
+        System.out.println("-------------Original List ------------------");
+        printList(head);
+
+        System.out.println("--------------The mid deletor----------------");
+        head = deletelast(head);
+        printList(head);
+    }
+
+    private static ListNode deletelast(ListNode head)
+    {
+        ListNode temp = head;
+        while(temp.next.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+
+}
