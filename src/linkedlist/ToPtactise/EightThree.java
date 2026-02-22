8e1b494e1884a8c5a44ba5f3890cae6ed53a8754
Abhinay Rana <abhinayrana46@gmail.com>
2026-02-22 11:47:51 +0530

    "Did the 83th question in the linkedList for the DSA"

---
+++ src/linkedlist/ToPtactise/EightThree.java
@@ -0,0 +1,33 @@
+package linkedlist.ToPtactise;
+
+public class EightThree {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    // This is the solution
+//    class Solution {
+//        public ListNode deleteDuplicates(ListNode head) {
+//            ListNode temp = head;
+//            while(temp != null && temp.next != null)
+//            {
+//                if(temp.val == temp.next.val)
+//                {
+//                    temp.next = temp.next.next;
+//                }
+//                else
+//                {
+//                    temp = temp.next;
+//                }
+//            }
+//            return head;
+//        }
+//    }
+}
