b60fc65f5052f4710030fa0eae27ba635bf7159e
Abhinay <abhinayrana46@gmail.com>
2026-03-05 14:45:50 +0530

    Did the 844th question of the String in a way better way.

--- src/String/EightFourFour.java
+++ src/String/EightFourFour.java
@@ -1,50 +1,26 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i = s.length() - 1;
-        int j = t.length() - 1;
-        int skipS = 0, skipT = 0;
+        String FinalS = toDoTheThing(s);
+        String FinalT = toDoTheThing(t);
 
-        while (i >= 0 || j >= 0) {
+        return FinalS.equals(FinalT);
+    }
 
-            // Process s
-            while (i >= 0) {
-                if (s.charAt(i) == '#') {
-                    skipS++;
-                    i--;
-                } else if (skipS > 0) {
-                    skipS--;
-                    i--;
-                } else {
-                    break;
-                }
+    private String toDoTheThing(String str)
+    {
+        StringBuilder stack = new StringBuilder();
+
+        for(char c : str.toCharArray())
+        {
+            if(c != '#')
+            {
+                stack.append(c);
             }
-
-            // Process t
-            while (j >= 0) {
-                if (t.charAt(j) == '#') {
-                    skipT++;
-                    j--;
-                } else if (skipT > 0) {
-                    skipT--;
-                    j--;
-                } else {
-                    break;
-                }
+            else if(stack.length() > 0)
+            {
+                stack.deleteCharAt(stack.length() - 1);
             }
-
-            // Compare characters
-            if (i >= 0 && j >= 0) {
-                if (s.charAt(i) != t.charAt(j)) {
-                    return false;
-                }
-            } else if (i >= 0 || j >= 0) {
-                return false;
-            }
-
-            i--;
-            j--;
         }
-
-        return true;
+        return stack.toString();
     }
 }
