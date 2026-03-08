e5b581b2346d48028f5f1c7c13ff32b48d06d4f8
Abhinay <abhinayrana46@gmail.com>
2026-03-08 05:50:30 +0530

    Did the 225 th question of Stack.

---
+++ src/Stack/TwoTwoFive.java
@@ -0,0 +1,30 @@
+class MyStack {
+
+    private java.util.Queue<Integer> queue;
+    public MyStack() {
+        queue = new java.util.LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+
+        int length = queue.size();
+        for(int i = 0; i < length - 1; i++)
+        {
+            int oldFront = queue.remove();
+            queue.add(oldFront);
+        }
+    }
+
+    public int pop() {
+        return queue.remove();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
