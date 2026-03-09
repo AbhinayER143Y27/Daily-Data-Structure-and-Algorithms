dd89495b421b35e32d254577f55c3b705a8b0d69
Abhinay <abhinayrana46@gmail.com>
2026-03-09 20:02:40 +0530

    Did the 153 th question of the binary search algo.

--- src/Stack/TwoTwoFive.java
+++ src/Stack/TwoTwoFive.java
@@ -4,7 +4,6 @@ class MyStack {
     public MyStack() {
         queue = new java.util.LinkedList<>();
     }
-
     public void push(int x) {
         queue.add(x);
 
@@ -15,16 +14,14 @@ class MyStack {
             queue.add(oldFront);
         }
     }
-
+    public boolean empty() {
+        return queue.isEmpty();
+    }
     public int pop() {
         return queue.remove();
     }
 
     public int top() {
         return queue.peek();
-    }
-
-    public boolean empty() {
-        return queue.isEmpty();
     }
 }
