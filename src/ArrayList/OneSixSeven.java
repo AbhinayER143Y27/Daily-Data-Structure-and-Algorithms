cf7024569bf6a6fa74699b10ec3d582d4b813c1b
Abhinay <abhinayrana46@gmail.com>
2026-03-25 15:30:32 +0530

    Did the 167th question of the Array.

---
+++ src/ArrayList/OneSixSeven.java
@@ -0,0 +1,26 @@
+package ArrayList;
+
+public class OneSixSeven {
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int low = 0;
+            int high = numbers.length - 1;
+            while(low <= high)
+            {
+                if(numbers[low] + numbers[high] == target)
+                {
+                    return new int[]{low + 1,high + 1};
+                }
+                else if(numbers[low] + numbers[high] < target)
+                {
+                    low++;
+                }
+                else if(numbers[low] + numbers[high] > target)
+                {
+                    high--;
+                }
+            }
+            return new int[]{-1,-1};
+        }
+    }
+}
