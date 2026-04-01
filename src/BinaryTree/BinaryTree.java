65001aada28808a8f4af7d16d602feb74c650ac9
Abhinay <abhinayrana46@gmail.com>
2026-04-01 18:18:58 +0530

    Started the binary Tree in the DataStructures.

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,54 @@
+package BinaryTree;
+
+import com.sun.source.tree.Tree;
+
+class TreeNode
+{
+    int val;
+    TreeNode leftNode;
+    TreeNode rightNode;
+
+    TreeNode(int val)
+    {
+        this.val = val;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+}
+public class BinaryTree
+{
+    TreeNode root;
+
+    public BinaryTree()
+    {
+        this.root = null;
+    }
+    public void insert(int val)
+    {
+        root = insertRec(val, root);
+    }
+
+    private TreeNode insertRec(int val , TreeNode root)
+    {
+        if(root == null)
+        {
+            root = new TreeNode(val);
+            return root;
+        }
+
+        else if(val < root.val)
+        {
+            root.leftNode = insertRec(val,root.leftNode);
+        }
+
+        else if(val > root.val)
+        {
+            root.rightNode = insertRec(val,root.rightNode);
+        }
+
+        return root;
+    }
+
+
+
+}
