88224b13d4f9e469381bad71190baeff9647b012
Abhinay <abhinayrana46@gmail.com>
2026-04-07 20:17:32 +0530

    This is a an order of the ways to print the nodes of the Binary Search Tree.

--- src/BinaryTree/OrdersOfDFS.java
+++ src/BinaryTree/OrdersOfDFS.java
@@ -1,42 +1,42 @@
-package BinaryTree;
-
-public class OrdersOfDFS {
-    public void PreOrder()
-    {
-        PreOrderRec(root);
-    }
-
-    private void PreOrderRec(TreeNode root)
-    {
-        if(root != null)
-        {
-            System.out.println(root.val + " ");
-            PreOrderRec(root.left);
-            PreOrderRec(root.right);
-        }
-    }
-
-    public void inOrder()
-    {
-        inOrderRec(root);
-    }
-
-    private void inOrderRec(TreeNode root)
-    {
-        inOrderRec(root.left);
-        System.out.println(root.val + " ");
-        inOrderRec(root.right);
-    }
-
-    public void postOrder()
-    {
-        postOrderRec(root);
-    }
-
-    private void postOrderRec(TreeNode root)
-    {
-        postOrderRec(root.left);
-        postOrderRec(root.right);
-        System.out.println(root.val + " ");
-    }
-}
+//package BinaryTree;
+//
+//public class OrdersOfDFS {
+//    public void PreOrder()
+//    {
+//        PreOrderRec(root);
+//    }
+//
+//    private void PreOrderRec(TreeNode root)
+//    {
+//        if(root != null)
+//        {
+//            System.out.println(root.val + " ");
+//            PreOrderRec(root.left);
+//            PreOrderRec(root.right);
+//        }
+//    }
+//
+//    public void inOrder()
+//    {
+//        inOrderRec(root);
+//    }
+//
+//    private void inOrderRec(TreeNode root)
+//    {
+//        inOrderRec(root.left);
+//        System.out.println(root.val + " ");
+//        inOrderRec(root.right);
+//    }
+//
+//    public void postOrder()
+//    {
+//        postOrderRec(root);
+//    }
+//
+//    private void postOrderRec(TreeNode root)
+//    {
+//        postOrderRec(root.left);
+//        postOrderRec(root.right);
+//        System.out.println(root.val + " ");
+//    }
+//}
