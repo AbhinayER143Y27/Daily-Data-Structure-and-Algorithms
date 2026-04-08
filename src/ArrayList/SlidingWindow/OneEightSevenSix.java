5e436cd418ba5c694217dddb291314669a84412c
Abhinay <abhinayrana46@gmail.com>
2026-04-08 19:11:15 +0530

    Did the 1876 th question from the LeetCode for the Daily Practise of the DSA.

---
+++ src/ArrayList/SlidingWindow/OneEightSevenSix.java
@@ -0,0 +1,18 @@
+package ArrayList.SlidingWindow;
+
+public class OneEightSevenSix {
+    class Solution {
+        public int countGoodSubstrings(String s) {
+            int counter = 0;
+            for(int i = 0; i < s.length() - 2; i++)
+            {
+                if(s.charAt(i) != s.charAt(i+1) && s.charAt(i + 1) != s.charAt(i+2) && s.charAt(i) != s.charAt(i+2))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+
+        }
+    }
+}
