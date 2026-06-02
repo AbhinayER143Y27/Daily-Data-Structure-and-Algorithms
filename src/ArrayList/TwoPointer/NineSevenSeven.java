c68dcea559d7342fd7afa614273a3126bba4cd62
Abhinay <abhinayrana46@gmail.com>
2026-06-02 14:54:46 +0530

    Did the 977th question from the LeetCode for the Daily Practise of the DSA using the Two Pointer approach.

---
+++ src/ArrayList/TwoPointer/NineSevenSeven.java
@@ -0,0 +1,29 @@
+package ArrayList.TwoPointer;
+
+public class NineSevenSeven {
+    class Solution {
+        public int[] sortedSquares(int[] nums) {
+            int[] nums2 = new int[nums.length];
+            int left = 0, right = nums.length - 1;
+            int index = nums.length - 1;
+            while(left <= right)
+            {
+                int leftSqr = nums[left] * nums[left];
+                int rightSqr = nums[right] * nums[right];
+                if(leftSqr > rightSqr)
+                {
+                    nums2[index] = leftSqr;
+                    left++;
+                    index--;
+                }
+                else
+                {
+                    nums2[index] = rightSqr;
+                    right--;
+                    index--;
+                }
+            }
+            return nums2;
+        }
+    }
+}
