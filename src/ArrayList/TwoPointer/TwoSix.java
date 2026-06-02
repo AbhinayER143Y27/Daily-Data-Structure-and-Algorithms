80575e45544386fd5b8dd2f9926560b97997b16d
Abhinay <abhinayrana46@gmail.com>
2026-06-02 14:21:50 +0530

    Did the 26 th question from the LeetCode for the Daily Practise of the DSA.

---
+++ src/ArrayList/TwoPointer/TwoSix.java
@@ -0,0 +1,22 @@
+package ArrayList.TwoPointer;
+
+public class TwoSix {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int left = 0, right = 1;
+            while(right < nums.length)
+            {
+                if(nums[left] == nums[right])
+                {
+                    right++;
+                }
+                else
+                {
+                    nums[left + 1] = nums[right];
+                    left++;
+                }
+            }
+            return left + 1;
+        }
+    }
+}
