1010751ea69f8e99f9545f4ef3b840fc5f6c7f43
Abhinay <abhinayrana46@gmail.com>
2026-06-09 10:12:06 +0530

    Did the Min Size Sub Array using the Sliding Window as to make progress in the Data Structure and Algorithms.

---
+++ src/ArrayList/SlidingWindow/MinSizeSubarray.java
@@ -0,0 +1,24 @@
+package ArrayList.SlidingWindow;
+
+public class MinSizeSubarray {
+
+    class Solution {
+        public int minSubArrayLen(int target, int[] nums) {
+            int minLength = Integer.MAX_VALUE;
+            int windowSum = 0;
+            int left = 0;
+            for(int right = 0; right < nums.length; right++)
+            {
+                windowSum += nums[right];
+                while(windowSum >= target)
+                {
+                    int length = (right - left + 1);
+                    minLength = Math.min(length, minLength);
+                    windowSum = windowSum - nums[left];
+                    left++;
+                }
+            }
+            return minLength == Integer.MAX_VALUE? 0 : minLength;
+        }
+    }
+}
