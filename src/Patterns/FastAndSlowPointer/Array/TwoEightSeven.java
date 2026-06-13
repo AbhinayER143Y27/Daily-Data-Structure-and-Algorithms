1c8aae2aa6f7ee56b78de351b6ad88662484c62c
Abhinay <abhinayrana46@gmail.com>
2026-06-13 20:01:41 +0530

    Did the 287th question using the Fast and the Slow pointer approach.

---
+++ src/Patterns/FastAndSlowPointer/Array/TwoEightSeven.java
@@ -0,0 +1,26 @@
+package Patterns.FastAndSlowPointer.Array;
+
+public class TwoEightSeven {
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            int slow = nums[0];
+            int fast = nums[0];
+
+            do
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            while(slow != fast);
+
+            slow = nums[0];
+
+            while(slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return slow;
+        }
+    }
+}
