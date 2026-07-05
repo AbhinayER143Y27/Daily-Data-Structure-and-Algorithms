77b75773c5ba1fd66456a6394519298be848432b
Abhinay <abhinayrana46@gmail.com>
2026-07-05 20:30:59 +0530

    Did the 20th question.

---
+++ src/Patterns/Stack/TwoZero.java
@@ -0,0 +1,31 @@
+package Patterns.Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TwoZero {
+    class Solution {
+        public boolean isValid(String s) {
+            Deque<Character> stack = new ArrayDeque<>();
+            for(char ch : s.toCharArray())
+            {
+                if(ch == '(' || ch == '{' || ch == '[')
+                {
+                    stack.push(ch);
+                }
+                else
+                {
+                    if(stack.isEmpty())
+                    {
+                        return false;
+                    }
+                    char top = stack.pop();
+                    if(ch == ')' && top != '(') return false;
+                    if(ch == '}' && top != '{') return false;
+                    if(ch == ']' && top != '[') return false;
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+}
