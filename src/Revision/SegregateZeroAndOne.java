dab36fcbe82ea715ca4c9bc72d19ab0b3a5b1c46
Abhinay <abhinayrana46@gmail.com>
2026-07-12 15:57:44 +0530

    Did the segregation of the 0 and the 1's question.

---
+++ src/Revision/SegregateZeroAndOne.java
@@ -0,0 +1,23 @@
+package Revision;
+
+public class SegregateZeroAndOne {
+    class Solution {
+        void segregate0and1(int[] arr) {
+            int left = 0;
+
+            for(int right = 0; right < arr.length; right++)
+            {
+                if(arr[right] == 0)
+                {
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+                    left++;
+                }
+            }
+
+        }
+    }
+
+}
