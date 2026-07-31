21c0facc8bce6cc6f2795c1d19eab053cd1b2c3f
Abhinay <abhinayrana46@gmail.com>
2026-07-31 10:46:31 +0530

    Did the 287th question from the leet code.

---
+++ src/Revision/TwoEightSeven.java
@@ -0,0 +1,25 @@
+package Revision;
+
+public class TwoEightSeven {
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            int slow = nums[0];
+            int fast = nums[0];
+
+            do
+            {
+                slow = nums[nums[slow]];
+                fast = nums[nums[nums[fast]]];
+            }while(slow != fast);
+
+            slow = nums[0];
+
+            while(slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return fast;
+        }
+    }
+}
