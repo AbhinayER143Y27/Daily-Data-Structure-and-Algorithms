4c67c8ce47640b33b1c59535a13e6dccbca1d1bc
Abhinay <abhinayrana46@gmail.com>
2026-09-12 17:57:14 +0530

    Did the 71th question.

--- src/Stack/SevenOne.java
+++ src/Stack/SevenOne.java
@@ -1,33 +1,39 @@
-//class Solution {
-//    public String simplifyPath(String path) {
-//        Deque<String> stack = new ArrayDeque<>();
-//        String[] tokens  = path.split("/");
-//
-//        for(String token : tokens)
-//        {
-//            if(token.equals(".."))
-//            {
-//                if(!stack.isEmpty())
-//                {
-//                    stack.pop();
-//                }
-//            }
-//            else if(!token.isEmpty() && !token.equals("."))
-//            {
-//                stack.push(token);
-//            }
-//        }
-//        if(stack.isEmpty())
-//        {
-//            return "/";
-//        }
-//        StringBuilder finalPath = new StringBuilder();
-//        Iterator<String> it = stack.descendingIterator();
-//
-//        while(it.hasNext())
-//        {
-//            finalPath.append("/").append(it.next());
-//        }
-//        return finalPath.toString();
-//    }
-//}
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+class SevenOne {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] tokens  = path.split("/");
+
+        for(String token : tokens)
+        {
+            if(token.equals(".."))
+            {
+                if(!stack.isEmpty())
+                {
+                    stack.pop();
+                }
+            }
+            else if(!token.isEmpty() && !token.equals("."))
+            {
+                stack.push(token);
+            }
+        }
+        if(stack.isEmpty())
+        {
+            return "/";
+        }
+        StringBuilder finalPath = new StringBuilder();
+        Iterator<String> it = stack.descendingIterator();
+
+        while(it.hasNext())
+        {
+            finalPath.append("/").append(it.next());
+        }
+        return finalPath.toString();
+    }
+}
